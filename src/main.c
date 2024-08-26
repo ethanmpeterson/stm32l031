5aea0767482cbaf3b3074334083763d39cd4b590
Ethan Peterson <ethan@petetech.net>
2024-08-25 20:38:09 -0700

    Base RTC initialization and loading a default timestamp

--- src/main.c
+++ src/main.c
@@ -16,6 +16,9 @@
 #include "hal_gpio.h"
 #include "hal_gpio_microSpecific.h"
 
+#include "hal_rtc.h"
+#include "hal_rtc_microSpecific.h"
+
 // Device / Board Layer Imports
 
 // Application Layer Imports
@@ -40,7 +43,27 @@ static void blinky_task(void *pvParameters) {
 static void uart_task(void *pvParameters) {
   for (;;) {
     // Transfer the byte if the peripheral is available
-    (void)hal_uart_sendByte(HAL_UART_CHANNEL_COM_PORT, 'n');
+    hal_rtc_time_S currentTime;
+    currentTime.seconds = 0;
+    (void)hal_rtc_microSpecific_getTime(&currentTime);
+    char secondsTens = (char)((currentTime.seconds / 10) % 10) + '0';
+    char secondsUnits = (char)(currentTime.seconds % 10) + '0';
+    char minutesTens = (char)((currentTime.minute / 10) % 10) + '0';
+    char minutesUnits = (char)(currentTime.minute % 10) + '0';
+    char hoursTens = (char)((currentTime.hour / 10) % 10) + '0';
+    char hoursUnits = (char)(currentTime.hour % 10) + '0';
+
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, hoursTens);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, hoursUnits);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, ':');
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, minutesTens);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, minutesUnits);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, ':');
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, secondsTens);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, secondsUnits);
+    (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, '\n');
+
+    /* (void)hal_uart_sendChar(HAL_UART_CHANNEL_COM_PORT, ':'); */
 
     // Higher priority task than the blinky
     // Give some time for the blinky to run
@@ -51,14 +74,17 @@ static void uart_task(void *pvParameters) {
 int main(void) {
   (void)hal_init();
 
+  (void)hal_rtc_microSpecific_init();
   (void)hal_gpio_microSpecific_init();
   (void)hal_uart_microSpecific_init();
 
-  xTaskHandle xHandleTask1;
-  xTaskCreate(blinky_task, "blinky", 50, NULL, tskIDLE_PRIORITY,
-              &xHandleTask1);
-  xTaskCreate(uart_task, "uart", 50, NULL, tskIDLE_PRIORITY + 1,
-              &xHandleTask1);
+  // These handles are used to identify the tasks and reference them.
+  // Suspend, resume, notify etc.
+  xTaskHandle blinkyTaskHandle;
+  xTaskHandle uartTaskHandle;
+
+  xTaskCreate(blinky_task, "blinky", 50, NULL, tskIDLE_PRIORITY, &blinkyTaskHandle);
+  xTaskCreate(uart_task, "uart", 50, NULL, tskIDLE_PRIORITY + 1, &uartTaskHandle);
 
   vTaskStartScheduler();
 
