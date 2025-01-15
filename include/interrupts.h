f40b2ef1866612f6d296a0e1f9db9299b9c66a5b
Ethan Peterson <ethan@petetech.net>
2025-01-14 23:34:42 -0800

    Bring up USART Interrupts
    TODO:
    - Add ability to register an interrupt handler for bytes received
    - Add ability to register an interrupt handler for bytes sent
    - Add handler for general peripheral faults

---
+++ include/interrupts.h
@@ -0,0 +1,17 @@
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
+#include "hal.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void USART2_IRQHandler(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* INTERRUPTS_H */
